c1f16f74d979ddf4ed47bfe60fa055bf75d25625
hxhjfgig8686 <ahmedalselwi0431@gmail.com>
2026-03-24 01:23:59 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <?php
 // admin/login.php
 session_start();
-require_once __DIR__ . '/../api/auth.php';
+require_once __DIR__ . '/../includes/auth.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'] ?? '';
